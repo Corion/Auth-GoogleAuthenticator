74512ad0c5a5393f7b9fa7a6a4c4108810da1560
Max Maischein <corion@corion.net>
2012-09-15 20:08:41 +0200

    Basic two-factor login works

--- lib/Dancer/Auth/GoogleAuthenticator.pm
+++ lib/Dancer/Auth/GoogleAuthenticator.pm
@@ -1,18 +1,26 @@
 package Dancer::Auth::GoogleAuthenticator;
 use Dancer ':syntax';
+use Dancer::Plugin::FlashMessage;
+use Auth::GoogleAuthenticator;
+
+=head1 NAME
+
+Dancer::Auth::GoogleAuthenticator - Two-Factor demo app
+
+=cut
 
 our $VERSION = '0.01';
 
 my %users = (
-    { name => 'test',
+    test => { name => 'test',
       pass => 'test',
-      otp_secret => 'abcde12345',
+      otp_secret => 'test@example.com',
     },
-    { name => 'test2',
+    test2 => { name => 'test2',
       pass => 'test2',
       otp_secret => '',
     },
-    { name => 'admin',
+    admin => { name => 'admin',
       pass => 'admin',
       otp_secret => 'abcde123456',
     },
@@ -24,24 +32,34 @@ sub get_otp_auth {
     
     return unless $user;
     
-    my ($otp_secret) = $users{$user}->{otp_secret};
+    my ($otp_secret) = $user->{otp_secret};
     if( $otp_secret ) {
-        return Authen::GoogleAuthenticator->new( secret => $otp_secret );
+        return Auth::GoogleAuthenticator->new( secret => $otp_secret );
     };
     return
 };
 
 get '/' => sub {
     template 'index', {
-        user => session('user'),
-        twofactor_active => get_otp_auth(session('user')),
-        twofactor_available => session('twofactor'),
+        (user => session('user')),
+        #(twofactor_available => get_otp_auth(session('user') || '')),
+        #(twofactor_active => session('twofactor')),
     };
 };
 
 # Force authentication for all non-index pages
-before '/' => sub {
-    ...
+hook before => sub {
+    return
+        if request->path_info =~ m{^/(css|javascripts|400|500|favicon.ico|$)};
+    if (! session('user') && request->path_info !~ m{^/auth/login}) {
+        # XXX We should store the redirect target internally
+        # and give the user
+        # not an URL but an internal session as the redirect target
+        var requested_path => request->path_info;
+        request->path_info('/auth/login');
+        # We should redirect here!
+        redirect '/auth/login';
+    }
 };
 
 get '/auth/setup' => sub {
@@ -52,10 +70,12 @@ get '/auth/setup' => sub {
     
     # Display otp_secret if we have it
     # XXX Maybe this should be over SSH only
-    template 'setup_otp', {
+    template 'setup_twofactor', {
+        auth => $auth,
+        user => $user,
         otp_secret => $otp_secret
     };
-}
+};
 
 post '/auth/setup' => sub {
     my ($user) = session->{user};
@@ -73,47 +93,61 @@ post '/auth/setup' => sub {
     };
     
     redirect '/auth/setup';
-}
+};
 
 get '/auth/login' => sub {
-    my $return = params->{return} || '';
-    template 'login', { return => params->{return} };
-}
+    my $return = vars->{requested_path} || '';
+    template 'login';
+};
 
 # Maybe also have "requires('password')"
 #                 "requires('twofactor')"
+# XXX use session flash instead of session
 post '/auth/login' => sub {
-    my ($user_id,$pass,$otp) = params->{user}, params->{pass}, params->{otp};
-    my $return = params->{return} || '';
+    my ($user_id,$pass,$otp) = (params->{user}, params->{pass}, params->{otp});
+    my $return = vars->{requested_path} || '';
     
-    my $user = $users{ $user_id };
-    
-    if( $pass eq $user->{pass} ) {
+    my $user= $users{ $user_id };
+    if(     $user and $user->{pass}
+        and $pass and $pass eq $user->{pass} ) {
         my $auth_twofactor= get_otp_auth( $user );
         if( $auth_twofactor ) {
             # Check OTP in addition to password
-            if( 1 == $auth->verify( $otp )) {
+            if( $auth_twofactor->verify( $otp )) {
                 session 'user' => $user;
                 session 'twofactor' => 1;
+                flash success => "User logged in with two-factor auth";
+                warning "User '$user_id' logged in with two-factor auth";
             } else {
                 # Log the failure
                 # Increment a failure counter for that user
                 # Increment a failure counter for that IP
                 # Increment a failure counter overall, to detect clock skew
+                warning "Wrong OTP for user '$user_id'";
+                flash error => "User unknown or wrong password/OTP";
             };
         } else {
+            warning "User '$user_id' logged in with password";
             session 'user' => $user;
             session 'twofactor' => 0;
+            flash success => "User logged in with password auth";
         };
+    } else {
+        warning "Wrong password for user '$user_id'";
+        flash error => "User unknown or wrong password/OTP";
     };
     
     return redirect $return
         if( session('user') and $return );
     template 'login';
-}
+};
 
 get '/auth/logout' => sub {
-    session('user', undef);
+    session->destroy; # boom
+};
+
+get '/' => sub {
+    template 'index';
 };
 
 true;
